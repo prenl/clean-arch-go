da21ef1d825788ae85e0fcfa054af2e40a628b72
Yelnur Abdrakhmanov <79540438+prenl@users.noreply.github.com>
2024-02-16 21:53:03 +0600

    feat(day2): add database connection

--- services/contact/cmd/app/main.go
+++ services/contact/cmd/app/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"architecture_go/pkg/store/postgres"
+	"context"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	db, err := postgres.Connect("localhost", 5432, "postgres", "1112", "clean-arch-go")
+
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+
+	defer db.Close(context.Background())
+
+	log.Println("Connected to the database")
 }

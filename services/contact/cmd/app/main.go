e374cae44d029e48041a971e92cdc4bbe696d4d9
Yelnur Abdrakhmanov <79540438+prenl@users.noreply.github.com>
2024-02-16 22:48:37 +0600

    feat(contact): add http request listener

--- services/contact/cmd/app/main.go
+++ services/contact/cmd/app/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"architecture_go/pkg/store/postgres"
+	"architecture_go/services/contact/internal/delivery"
+	"architecture_go/services/contact/internal/repository"
+	"architecture_go/services/contact/internal/usecase"
 	"context"
 	"log"
+	"net/http"
 )
 
 func main() {
@@ -14,6 +18,17 @@ func main() {
 	}
 
 	defer db.Close(context.Background())
-
 	log.Println("Connected to the database")
+
+	repo := repository.NewContactRepository()
+	usecase := usecase.NewContactUseCase(repo)
+	delivery := delivery.NewContactDelivery(usecase)
+
+	_ = delivery
+
+	log.Println("Server is starting on port :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Failed to start the server: %v", err)
+	}
+
 }

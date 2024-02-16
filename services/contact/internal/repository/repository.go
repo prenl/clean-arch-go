9d5673b3f00f95064c69461747dc3eaadf52f5f8
Yelnur Abdrakhmanov <79540438+prenl@users.noreply.github.com>
2024-02-16 22:29:18 +0600

    feat(contact): create repository interface

---
+++ services/contact/internal/repository/repository.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"architecture_go/services/contact/internal/domain"
+)
+
+type ContactRepository interface {
+	// Contact model
+	CreateContact(contact domain.Contact) (int, error)
+	GetContact(id int) (*domain.Contact, error)
+	UpdateContact(contact domain.Contact) error
+	DeleteContact(id int) error
+
+	// Group model
+	CreateGroup(group domain.Group) (int, error)
+	GetGroup(id int) (*domain.Group, error)
+	UpdateGroup(group domain.Group) error
+	DeleteGroup(id int) error
+
+	// ContactGroup model
+	AddContactToGroup(contactID, groupID int) error
+	RemoveContactFromGroup(contactID, groupID int) error
+	GetContactsByGroup(groupID int) ([]*domain.Contact, error)
+}
